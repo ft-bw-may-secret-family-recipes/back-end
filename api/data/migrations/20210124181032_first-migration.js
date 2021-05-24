a16994e68ff5876551502de30cb933472a740925
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-24 11:57:25 -0500

    Reverse Migrate Down, Begin users-model

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,56 +1,56 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (table) => {
-      table.increments('user_id')
-      table.string('user_username', 200).notNullable()
-      table.string('user_password', 200).notNullable()
-      table.string('user_email', 320).notNullable()
-      table.timestamps(false, true)
+    .createTable("users", (table) => {
+      table.increments("user_id");
+      table.string("user_username", 200).notNullable().unique();
+      table.string("user_password", 200).notNullable();
+      table.string("user_email", 320).notNullable().unique();
+      table.timestamps(false, true);
     })
 
-    .createTable('recipes', (table) => {
-      table.increments('recipe_id');
-      table.string('recipe_name', 128).notNullable().unique();//should this be unique
+    .createTable("recipes", (table) => {
+      table.increments("recipe_id");
+      table.string("recipe_name", 128).notNullable().unique(); //should this be unique
       table
-        .integer('user_id')
+        .integer("user_id")
         .unsigned()
         .notNullable()
-        .references('user_id')
-        .inTable('users')
-        .onUpdate('RESTRICT')
-        .onDelete('RESTRICT');
+        .references("user_id")
+        .inTable("users")
+        .onUpdate("RESTRICT")
+        .onDelete("RESTRICT");
     })
 
-    .createTable('steps', (table) => {
-      table.increments('step_id');
-      table.text('instruction_description').notNullable();
+    .createTable("steps", (table) => {
+      table.increments("step_id");
+      table.text("instruction_description").notNullable();
       table
-        .integer('recipe_id')
+        .integer("recipe_id")
         .unsigned()
         .notNullable()
-        .references('recipe_id')
-        .inTable('recipes')
-        .onUpdate('RESTRICT')
-        .onDelete('RESTRICT');
+        .references("recipe_id")
+        .inTable("recipes")
+        .onUpdate("RESTRICT")
+        .onDelete("RESTRICT");
     })
 
-    .createTable('ingredients', (table) => {
-      table.increments('ingredient_id');
-      table.string('ingredient_name', 128).notNullable().unique();
+    .createTable("ingredients", (table) => {
+      table.increments("ingredient_id");
+      table.string("ingredient_name", 128).notNullable().unique();
       table
-        .integer('step_id')
+        .integer("step_id")
         .unsigned()
         .notNullable()
-        .references('step_id')
-        .inTable('steps')
-        .onUpdate('RESTRICT')
-        .onDelete('RESTRICT');
+        .references("step_id")
+        .inTable("steps")
+        .onUpdate("RESTRICT")
+        .onDelete("RESTRICT");
     });
-}
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-  await knex.schema.dropTableIfExists('recipes')
-  await knex.schema.dropTableIfExists('steps')
-  await knex.schema.dropTableIfExists('ingredients')
-}
+  await knex.schema.dropTableIfExists("ingredients");
+  await knex.schema.dropTableIfExists("steps");
+  await knex.schema.dropTableIfExists("recipes");
+  await knex.schema.dropTableIfExists("users");
+};
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,1 +1,16 @@
 const db = require("../data/db-config");
+
+const getAllUsers = () => db("users");
+
+const getUserById = () => db("users");
+
+const addUser = async (user) => {
+  const newUser = await db("users").insert(user).returning("*");
+  return newUser[0];
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  addUser,
+};
