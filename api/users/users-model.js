c640572face5400ad4306c91aa09e7b8df3a91a4
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-24 14:25:54 -0500

    Refractor Names Of users-model Functions

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,12 +1,12 @@
 const db = require("../data/db-config");
 
-const getAllUsers = () => db("users"); //admin only
+const getAll = () => db("users"); //admin only
 
-const getUserById = (id) => db("users").where("user_id", id).first(); //matching user token required
+const getById = (id) => db("users").where("user_id", id).first(); //matching user token required
 
-const getUserBy = (userProp) => db("users").where(userProp);
+const getBy = (userProp) => db("users").where(userProp);
 
-const updateUser = async (id, updateUser) => {
+const update = async (id, updateUser) => {
   const updatedUser = db("users")
     .where("user_id", id)
     .update(updateUser)
@@ -15,15 +15,15 @@ const updateUser = async (id, updateUser) => {
 };
 //matching user token required
 
-const addUser = async (user) => {
+const add = async (user) => {
   const newUser = await db("users").insert(user).returning("*");
   return newUser[0];
 }; //via registration
 
 module.exports = {
-  getAllUsers,
-  getUserById,
-  addUser,
-  getUserBy,
-  updateUser,
+  getAll,
+  getById,
+  add,
+  getBy,
+  update,
 };
