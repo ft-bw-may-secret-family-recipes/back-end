361e8ef70a58fcd437a0a19302c252bd0439e24f
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-24 14:20:47 -0500

    Refractor addRecipe To Return Object Instead Of Array

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,18 +1,24 @@
 const db = require("../data/db-config");
 
-const getAllUsers = () => db("users");
+const getAllUsers = () => db("users"); //admin only
 
-const getUserById = (id) => db("users").where("user_id", id).first();
+const getUserById = (id) => db("users").where("user_id", id).first(); //matching user token required
 
 const getUserBy = (userProp) => db("users").where(userProp);
 
-const updateUser = async (id, updateUser) =>
-  db("users").where("user_id", id).update(updateUser).returning("*");
+const updateUser = async (id, updateUser) => {
+  const updatedUser = db("users")
+    .where("user_id", id)
+    .update(updateUser)
+    .returning("*");
+  return updatedUser[0];
+};
+//matching user token required
 
 const addUser = async (user) => {
   const newUser = await db("users").insert(user).returning("*");
   return newUser[0];
-};
+}; //via registration
 
 module.exports = {
   getAllUsers,
