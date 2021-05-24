2410d8ec871be1e95b84806c7f049af054f7efaa
Luis Garcia <jrluis21@gmail.com>
2021-05-24 15:47:25 -0500

    created category table and added to recipe table

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,56 +1,77 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (table) => {
-      table.increments('user_id')
-      table.string('user_username', 200).notNullable()
-      table.string('user_password', 200).notNullable()
-      table.string('user_email', 320).notNullable()
-      table.timestamps(false, true)
+    .createTable("users", (table) => {
+      table.increments("user_id");
+      table.string("user_username", 200).notNullable().unique();
+      table.string("user_password", 200).notNullable();
+      table.string("user_email", 320).notNullable().unique();
+      table.timestamps(false, true);
     })
 
-    .createTable('recipes', (table) => {
-      table.increments('recipe_id');
-      table.string('recipe_name', 128).notNullable().unique();//should this be unique
+    .createTable("recipes", (table) => {
+      table.increments("recipe_id");
+      table.string("recipe_name", 128).notNullable().unique();
+      table.string("recipe_source", 128).notNullable()
       table
-        .integer('user_id')
+        .integer("user_id")
         .unsigned()
         .notNullable()
-        .references('user_id')
-        .inTable('users')
-        .onUpdate('RESTRICT')
-        .onDelete('RESTRICT');
+        .references("user_id")
+        .inTable("users")
+        .onUpdate("RESTRICT")
+        .onDelete("RESTRICT");
     })
 
     .createTable('steps', (table) => {
       table.increments('step_id');
-      table.text('instruction_description').notNullable();
+      table.text('steps_description').notNullable();
       table
-        .integer('recipe_id')
+        .integer("recipe_id")
         .unsigned()
         .notNullable()
-        .references('recipe_id')
-        .inTable('recipes')
-        .onUpdate('RESTRICT')
-        .onDelete('RESTRICT');
+        .references("recipe_id")
+        .inTable("recipes")
+        .onUpdate("RESTRICT")
+        .onDelete("RESTRICT");
     })
 
     .createTable('ingredients', (table) => {
       table.increments('ingredient_id');
       table.string('ingredient_name', 128).notNullable().unique();
-      table
-        .integer('step_id')
+      table.string('ingredients_unit', 128).notNullable()
+    })
+
+    .createTable('step_ingredients', table => {
+      table.increments('step_ingredients_id')
+      table.float('quantity').notNullable()
+      table.integer('step_id')
         .unsigned()
         .notNullable()
         .references('step_id')
         .inTable('steps')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE')
+      table.integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('ingredient_id')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
         .onUpdate('RESTRICT')
-        .onDelete('RESTRICT');
+    })
+    .createTable('category', table => {
+      table.increments('category_id')
+      table.string('category').notNullable()
     });
+
+
+
 }
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-  await knex.schema.dropTableIfExists('recipes')
-  await knex.schema.dropTableIfExists('steps')
+  await knex.schema.dropTableIfExists('step_ingredients')
   await knex.schema.dropTableIfExists('ingredients')
+  await knex.schema.dropTableIfExists('steps')
+  await knex.schema.dropTableIfExists('recipes')
+  await knex.schema.dropTableIfExists('users')
 }
