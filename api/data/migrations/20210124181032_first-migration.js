7c1d9857d9248f290ebb2dfae2f8eb3793b69c50
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-24 16:54:49 -0500

    Pluralize Categories Table, Refer Same To Users Table

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -60,15 +60,15 @@ exports.up = async (knex) => {
         .onUpdate('RESTRICT')
     })
 
-    .createTable('category', table => {
+    .createTable('categories', table => {
       table.increments('category_id')
       table.string('category').notNullable()
       table
-        .integer("recipe_id")
+        .integer("user_id")
         .unsigned()
         .notNullable()
-        .references("recipe_id")
-        .inTable("recipes")
+        .references("user_id")
+        .inTable("users")
         .onUpdate("RESTRICT")
         .onDelete("RESTRICT");
     });
