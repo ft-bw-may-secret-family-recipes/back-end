8c9644bd57c29aa6d86c0bedad067a0128777584
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-25 11:12:15 -0500

    Export Middleware Via middleware/index.js

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -3,3 +3,5 @@ exports.checkUsernameAvailable = require("./check-username-available");
 exports.checkUserIdExists = require("./check-user-id-exists");
 
 exports.checkUsernameExists = require("./check-username-exists");
+
+exports.checkAdmin = require("./check-admin");
