4984697466f28ff772b11130b25ec1172a1692ae
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-26 17:13:16 -0500

    Refractor 2nd, Rename 3rd Migration

--- api/data/migrations/20210526013050_add-step_numbers-to-steps.js
+++ api/data/migrations/20210526013050_add-step_numbers-to-steps.js
@@ -1,7 +1,7 @@
 exports.up = async (knex) => {
-  await knex.schema.table("steps", (table) => {
-    table.integer("step_number").unsigned().notNullable();
-  });
+  await knex.schema.table("steps", (table) =>
+    table.integer("step_number").unsigned().notNullable()
+  );
 };
 
 exports.down = async (knex) => {
