1f49118a8b19216b90f7c60305e9b9d2c1bbe979
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-25 21:05:48 -0500

    Annotate recipes-router

--- api/middleware/check-admin.js
+++ api/middleware/check-admin.js
@@ -1,4 +1,4 @@
 const checkAdmin = async (req, res, next) => {
-  req.headers.auth === "5678" ? next() : res.end();
+  req.headers.authorization === "5678" ? next() : res.end();
 };
 module.exports = checkAdmin;
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -4,17 +4,19 @@ const Recipes = require("./recipes-model");
 
 router.use(checkAdmin);
 
-const Ingredients = require("./ingredients-model")
-const Categories = require('./categories-model')
+const Ingredients = require("./ingredients-model");
+const Categories = require("./categories-model");
 //////////RECIPES//////////
 
 router.get("/", (req, res, next) => {
+  // todo: move to admin router
   Recipes.getAll()
     .then((recipes) => res.status(200).json(recipes))
     .catch(next);
 });
 
 router.get("/:user_id", checkUserIdExists, (req, res, next) => {
+  // todo: users pulled from decoded token
   Recipes.getByUserId(req.params.user_id)
     .then((recipes) => {
       res.status(200).json(recipes);
@@ -23,12 +25,17 @@ router.get("/:user_id", checkUserIdExists, (req, res, next) => {
 });
 
 router.get("/:user_id/:recipe_id", checkUserIdExists, (req, res, next) => {
+  // todo: users pulled from decoded token
   const { user_id, recipe_id } = req.params;
-  Recipes.getBy(user_id, { recipe_id: recipe_id });
+  Recipes.getFull(user_id, recipe_id)
+    .then((recipe) => {
+      res.status(200).json(recipe);
+    })
+    .catch(next);
 });
 
-
 router.post("/:user_id/", checkUserIdExists, (req, res, next) => {
+  // todo: users pulled from decoded token
   Recipes.add(req.params.user_id, req.body)
     .then((recipe) => {
       res.status(201).json(recipe);
@@ -37,47 +44,43 @@ router.post("/:user_id/", checkUserIdExists, (req, res, next) => {
 });
 
 //////////INGREDIENTS//////////
-router.get('/ingredients', (req,res,next) => {
-    Ingredients.getAll()
-    .then(ingredients => {
-        res.json(ingredients)
+router.get("/ingredients", (req, res, next) => {
+  // todo: refractor into ingredients router
+  Ingredients.getAll()
+    .then((ingredients) => {
+      res.json(ingredients);
     })
-    .catch(next)
-})
-router.get('/ingredients/:ingredient_id',(req, res, next) => {
-    const { ingredient_id } = req.params
-  
-    Ingredients.getByIngredientId(ingredient_id)
-      .then(ingredients => {
-        res.json(ingredients)
-      })
-      .catch(next)
-  })
+    .catch(next);
+});
+router.get("/ingredients/:ingredient_id", (req, res, next) => {
+  // todo: refractor into ingredients router
+  const { ingredient_id } = req.params;
+
+  Ingredients.getByIngredientId(ingredient_id)
+    .then((ingredients) => {
+      res.json(ingredients);
+    })
+    .catch(next);
+});
 //////////CATEGORIES//////////
-router.get('/categories', (req,res,next) => {
-    Categories.getAll()
-    .then(categories => {
-        res.json(categories)
+router.get("/categories", (req, res, next) => {
+  // todo: refractor into admin router
+  Categories.getAll()
+    .then((categories) => {
+      res.json(categories);
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
-router.get('/categories/:user_id',(req, res, next) => {
-    const { user_id } = req.params
-  
-    Categories.getByUserId(user_id)
-      .then(categories => {
-        res.json(categories)
-      })
-      .catch(next)
-  })
+router.get("/categories/:user_id", (req, res, next) => {
+  // todo: refractor into categories router
+  const { user_id } = req.params;
 
-
-
-
-
-
-
-
+  Categories.getByUserId(user_id)
+    .then((categories) => {
+      res.json(categories);
+    })
+    .catch(next);
+});
 
 module.exports = router;
