d4e6011b57bb43a04ba6da584b445361f2aed058
Luis Garcia <jrluis21@gmail.com>
2021-05-26 08:13:50 -0500

    completed recipe router

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-
 const Recipe = require('./recipes-model')
 const Ingredients = require("./ingredients-model")
 const Categories = require('./categories-model')
@@ -11,7 +10,6 @@ router.get('/', (req,res,next) => {
     })
     .catch(next)
 })
-
 router.get('/:recipe_id',(req, res, next) => {
     const { recipe_id } = req.params
   
@@ -21,7 +19,6 @@ router.get('/:recipe_id',(req, res, next) => {
       })
       .catch(next)
   })
-
   router.post('/',(req, res, next) => {
       Recipe.add(req.body)
         .then((recipe) => {
@@ -32,6 +29,15 @@ router.get('/:recipe_id',(req, res, next) => {
         });
     }
   );
+  router.get('/:recipe_id',(req, res, next) => {
+    const { recipe_id } = req.params
+  
+    Recipe.getBy(recipe_id)
+      .then(recipe => {
+        res.json(recipe)
+      })
+      .catch(next)
+  })
 //////////INGREDIENTS//////////
 router.get('/ingredients', (req,res,next) => {
     Ingredients.getAll()
@@ -49,6 +55,16 @@ router.get('/ingredients/:ingredient_id',(req, res, next) => {
       })
       .catch(next)
   })
+
+  router.get('/ingredients/:ingredient_id',(req, res, next) => {
+    const { ingredient_id } = req.params
+  
+    Recipe.getBy(ingredient_id)
+      .then(recipe => {
+        res.json(recipe)
+      })
+      .catch(next)
+  })
 //////////CATEGORIES//////////
 router.get('/categories', (req,res,next) => {
     Categories.getAll()
@@ -57,7 +73,6 @@ router.get('/categories', (req,res,next) => {
     })
     .catch(next)
 })
-
 router.get('/categories/:user_id',(req, res, next) => {
     const { user_id } = req.params
   
@@ -67,38 +82,15 @@ router.get('/categories/:user_id',(req, res, next) => {
       })
       .catch(next)
   })
-=======
-const { checkAdmin, checkUserIdExists } = require("../middleware");
-const Recipes = require("./recipes-model");
 
-router.use(checkAdmin);
-
-router.get("/", (req, res, next) => {
-  Recipes.getAll()
-    .then((recipes) => res.status(200).json(recipes))
-    .catch(next);
-});
-
-router.get("/:user_id", checkUserIdExists, (req, res, next) => {
-  Recipes.getByUserId(req.params.user_id)
-    .then((recipes) => {
-      res.status(200).json(recipes);
-    })
-    .catch(next);
-});
-
-router.get("/:user_id/:recipe_id", checkUserIdExists, (req, res, next) => {
-  const { user_id, recipe_id } = req.params;
-  Recipes.getBy(user_id, { recipe_id: recipe_id });
-});
-
-router.post("/:user_id/", checkUserIdExists, (req, res, next) => {
-  Recipes.add(req.params.user_id, req.body)
-    .then((recipe) => {
-      res.status(201).json(recipe);
-    })
-    .catch(next);
-});
-
+  router.get('/categories/:user_id',(req, res, next) => {
+    const { user_id } = req.params
+  
+    Recipe.getBy(user_id)
+      .then(recipe => {
+        res.json(recipe)
+      })
+      .catch(next)
+  })
 
 module.exports = router;
